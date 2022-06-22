317d286552f7964b5ec6e5a3f8fcd4d0b49705f7
Andika Pranayoga <praandikayoga@gmail.com>
2022-06-22 16:14:41 +0800

    Edit link JS

--- hem/link.js
+++ hem/link.js
@@ -1,67 +1,67 @@
 let pathName = window.location.pathname;
 if (pathName == '/doni/') {
     // Doni
-    let button = document.write(`<a href="https://api.whatsapp.com/send?phone=6281246458509&text=Halo%20Kak%20Doni%2C%20saya%20dapat%20iklan%20dari%20website%2C%20mau%20tanya%20harga%20terbaru%20Yamaha%20Gear%20(https://gear125.yamahabismagroup.com/doni/)" class="btn" id="wa"><i class="fa-brands fa-whatsapp"></i> <br> Klaim Promo Sekarang</a>`);
+    let button = document.write(`<a href="https://api.whatsapp.com/send?phone=6281246458509&text=Halo%20Kak%20Doni%2C%20saya%20dapat%20iklan%20dari%20website%2C%20mau%20tanya%20harga%20terbaru%20Yamaha%20Gear%20(https://gear125.yamahabismagroup.com/doni/)" class="btn" id="wa"><i class="fa-brands fa-whatsapp"></i> Klaim Promo Sekarang</a>`);
     document.getElementById("wa").appendChild(button);
 
 } else if (pathName == '/nada/') {
     // Nada
-    let button = document.write(`<a href="https://api.whatsapp.com/send?phone=6281337558657&text=Halo%20Kak%20Nada%2C%20saya%20dapat%20iklan%20dari%20website%2C%20mau%20tanya%20harga%20terbaru%20Yamaha%20Gear%20(https://gear125.yamahabismagroup.com/nada/)" class="btn" id="wa"><i class="fa-brands fa-whatsapp"></i> <br> Klaim Promo Sekarang</a>`);
+    let button = document.write(`<a href="https://api.whatsapp.com/send?phone=6281337558657&text=Halo%20Kak%20Nada%2C%20saya%20dapat%20iklan%20dari%20website%2C%20mau%20tanya%20harga%20terbaru%20Yamaha%20Gear%20(https://gear125.yamahabismagroup.com/nada/)" class="btn" id="wa"><i class="fa-brands fa-whatsapp"></i> Klaim Promo Sekarang</a>`);
     document.getElementById("wa").appendChild(button);
 
 } else if (pathName == '/michael/') {
     // Michael
-    let button = document.write(`<a href="https://api.whatsapp.com/send?phone=6281337429691&text=Halo%20Kak%20Michael%2C%20saya%20dapat%20iklan%20dari%20website%2C%20mau%20tanya%20harga%20terbaru%20Yamaha%20Gear%20(https://gear125.yamahabismagroup.com/michael/)" class="btn" id="wa"><i class="fa-brands fa-whatsapp"></i> <br> Klaim Promo Sekarang</a>`);
+    let button = document.write(`<a href="https://api.whatsapp.com/send?phone=6281337429691&text=Halo%20Kak%20Michael%2C%20saya%20dapat%20iklan%20dari%20website%2C%20mau%20tanya%20harga%20terbaru%20Yamaha%20Gear%20(https://gear125.yamahabismagroup.com/michael/)" class="btn" id="wa"><i class="fa-brands fa-whatsapp"></i> Klaim Promo Sekarang</a>`);
     document.getElementById("wa").appendChild(button);
 
 } else if (pathName == '/yoga/') {
     // Yoga
-    let button = document.write(`<a href="https://api.whatsapp.com/send?phone=6281237165424&text=Halo%20Kak%20Yoga%2C%20saya%20dapat%20iklan%20dari%20website%2C%20mau%20tanya%20harga%20terbaru%20Yamaha%20Gear%20(https://gear125.yamahabismagroup.com/yoga/)" class="btn" id="wa"><i class="fa-brands fa-whatsapp"></i> <br> Klaim Promo Sekarang</a>`);
+    let button = document.write(`<a href="https://api.whatsapp.com/send?phone=6281237165424&text=Halo%20Kak%20Yoga%2C%20saya%20dapat%20iklan%20dari%20website%2C%20mau%20tanya%20harga%20terbaru%20Yamaha%20Gear%20(https://gear125.yamahabismagroup.com/yoga/)" class="btn" id="wa"><i class="fa-brands fa-whatsapp"></i> Klaim Promo Sekarang</a>`);
     document.getElementById("wa").appendChild(button);
 
 } else if (pathName == '/helmy/') {
     // Helmy
-    let button = document.write(`<a href="https://api.whatsapp.com/send?phone=6281946306260&text=Halo%20Kak%20Helmy%2C%20saya%20dapat%20iklan%20dari%20website%2C%20mau%20tanya%20harga%20terbaru%20Yamaha%20Gear%20(https://gear125.yamahabismagroup.com/helmy/)" class="btn" id="wa"><i class="fa-brands fa-whatsapp"></i> <br> Klaim Promo Sekarang</a>`);
+    let button = document.write(`<a href="https://api.whatsapp.com/send?phone=6281946306260&text=Halo%20Kak%20Helmy%2C%20saya%20dapat%20iklan%20dari%20website%2C%20mau%20tanya%20harga%20terbaru%20Yamaha%20Gear%20(https://gear125.yamahabismagroup.com/helmy/)" class="btn" id="wa"><i class="fa-brands fa-whatsapp"></i> Klaim Promo Sekarang</a>`);
     document.getElementById("wa").appendChild(button);
 
 } else if (pathName == '/agus/') {
     // Agus Mahardika
-    let button = document.write(`<a href="https://api.whatsapp.com/send?phone=62895630475382&text=Halo%20Kak%20Agus%2C%20saya%20dapat%20iklan%20dari%20website%2C%20mau%20tanya%20harga%20terbaru%20Yamaha%20Gear%20(https://gear125.yamahabismagroup.com/agus/)" class="btn" id="wa"><i class="fa-brands fa-whatsapp"></i> <br> Klaim Promo Sekarang</a>`);
+    let button = document.write(`<a href="https://api.whatsapp.com/send?phone=62895630475382&text=Halo%20Kak%20Agus%2C%20saya%20dapat%20iklan%20dari%20website%2C%20mau%20tanya%20harga%20terbaru%20Yamaha%20Gear%20(https://gear125.yamahabismagroup.com/agus/)" class="btn" id="wa"><i class="fa-brands fa-whatsapp"></i> Klaim Promo Sekarang</a>`);
     document.getElementById("wa").appendChild(button);
 
 } else if (pathName == '/dewangsa/') {
     // Dewangsa
-    let button = document.write(`<a href="https://api.whatsapp.com/send?phone=6281339510973&text=Halo%20Kak%20Dewangsa%2C%20saya%20dapat%20iklan%20dari%20website%2C%20mau%20tanya%20harga%20terbaru%20Yamaha%20Gear%20(https://gear125.yamahabismagroup.com/dewangsa/)" class="btn" id="wa"><i class="fa-brands fa-whatsapp"></i> <br> Klaim Promo Sekarang</a>`);
+    let button = document.write(`<a href="https://api.whatsapp.com/send?phone=6281339510973&text=Halo%20Kak%20Dewangsa%2C%20saya%20dapat%20iklan%20dari%20website%2C%20mau%20tanya%20harga%20terbaru%20Yamaha%20Gear%20(https://gear125.yamahabismagroup.com/dewangsa/)" class="btn" id="wa"><i class="fa-brands fa-whatsapp"></i> Klaim Promo Sekarang</a>`);
     document.getElementById("wa").appendChild(button);
 
 } else if (pathName == '/wahyu/') {
     // Wahyu
-    let button = document.write(`<a href="https://api.whatsapp.com/send?phone=6281805967888&text=Halo%20Kak%20Wahyu%2C%20saya%20dapat%20iklan%20dari%20website%2C%20mau%20tanya%20harga%20terbaru%20Yamaha%20Gear%20(https://gear125.yamahabismagroup.com/wahyu/)" class="btn" id="wa"><i class="fa-brands fa-whatsapp"></i> <br> Klaim Promo Sekarang</a>`);
+    let button = document.write(`<a href="https://api.whatsapp.com/send?phone=6281805967888&text=Halo%20Kak%20Wahyu%2C%20saya%20dapat%20iklan%20dari%20website%2C%20mau%20tanya%20harga%20terbaru%20Yamaha%20Gear%20(https://gear125.yamahabismagroup.com/wahyu/)" class="btn" id="wa"><i class="fa-brands fa-whatsapp"></i> Klaim Promo Sekarang</a>`);
     document.getElementById("wa").appendChild(button);
 
 } else if (pathName == '/rayhan/') {
     // Rayhan
-    let button = document.write(`<a href="https://api.whatsapp.com/send?phone=6285738919186&text=Halo%20Kak%20Rayhan%2C%20saya%20dapat%20iklan%20dari%20website%2C%20mau%20tanya%20harga%20terbaru%20Yamaha%20Gear%20(https://gear125.yamahabismagroup.com/rayhan/)" class="btn" id="wa"><i class="fa-brands fa-whatsapp"></i> <br> Klaim Promo Sekarang</a>`);
+    let button = document.write(`<a href="https://api.whatsapp.com/send?phone=6285738919186&text=Halo%20Kak%20Rayhan%2C%20saya%20dapat%20iklan%20dari%20website%2C%20mau%20tanya%20harga%20terbaru%20Yamaha%20Gear%20(https://gear125.yamahabismagroup.com/rayhan/)" class="btn" id="wa"><i class="fa-brands fa-whatsapp"></i> Klaim Promo Sekarang</a>`);
     document.getElementById("wa").appendChild(button);
 
 } else if (pathName == '/benz/') {
     // Benz
-    let button = document.write(`<a href="https://api.whatsapp.com/send?phone=6282145850330&text=Halo%20Kak%20Benz%2C%20saya%20dapat%20iklan%20dari%20website%2C%20mau%20tanya%20harga%20terbaru%20Yamaha%20Gear%20(https://gear125.yamahabismagroup.com/benz/)" class="btn" id="wa"><i class="fa-brands fa-whatsapp"></i> <br> Klaim Promo Sekarang</a>`);
+    let button = document.write(`<a href="https://api.whatsapp.com/send?phone=6282145850330&text=Halo%20Kak%20Benz%2C%20saya%20dapat%20iklan%20dari%20website%2C%20mau%20tanya%20harga%20terbaru%20Yamaha%20Gear%20(https://gear125.yamahabismagroup.com/benz/)" class="btn" id="wa"><i class="fa-brands fa-whatsapp"></i> Klaim Promo Sekarang</a>`);
     document.getElementById("wa").appendChild(button);
 
 } else if (pathName == '/surya/') {
     // Surya Ardika
-    let button = document.write(`<a href="https://api.whatsapp.com/send?phone=6281315581100&text=Halo%20Kak%20Surya%2C%20saya%20dapat%20iklan%20dari%20website%2C%20mau%20tanya%20harga%20terbaru%20Yamaha%20Gear%20(https://gear125.yamahabismagroup.com/surya/)" class="btn" id="wa"><i class="fa-brands fa-whatsapp"></i> <br> Klaim Promo Sekarang</a>`);
+    let button = document.write(`<a href="https://api.whatsapp.com/send?phone=6281315581100&text=Halo%20Kak%20Surya%2C%20saya%20dapat%20iklan%20dari%20website%2C%20mau%20tanya%20harga%20terbaru%20Yamaha%20Gear%20(https://gear125.yamahabismagroup.com/surya/)" class="btn" id="wa"><i class="fa-brands fa-whatsapp"></i> Klaim Promo Sekarang</a>`);
     document.getElementById("wa").appendChild(button);
 
 } else if (pathName == '/rajabath/') {
     // Rajabath
-    let button = document.write(`<a href="https://api.whatsapp.com/send?phone=62895340296207&text=Halo%20Kak%20Rajabath%2C%20saya%20dapat%20iklan%20dari%20website%2C%20mau%20tanya%20harga%20terbaru%20Yamaha%20Gear%20(https://gear125.yamahabismagroup.com/rajabath/)" class="btn" id="wa"><i class="fa-brands fa-whatsapp"></i> <br> Klaim Promo Sekarang</a>`);
+    let button = document.write(`<a href="https://api.whatsapp.com/send?phone=62895340296207&text=Halo%20Kak%20Rajabath%2C%20saya%20dapat%20iklan%20dari%20website%2C%20mau%20tanya%20harga%20terbaru%20Yamaha%20Gear%20(https://gear125.yamahabismagroup.com/rajabath/)" class="btn" id="wa"><i class="fa-brands fa-whatsapp"></i> Klaim Promo Sekarang</a>`);
     document.getElementById("wa").appendChild(button);
 
 } else if (pathName == '/adam/') {
     // Adam
-    let button = document.write(`<a href="https://api.whatsapp.com/send?phone=6281246776643&text=Halo%20Kak%20Adam%2C%20saya%20dapat%20iklan%20dari%20website%2C%20mau%20tanya%20harga%20terbaru%20Yamaha%20Gear%20(https://gear125.yamahabismagroup.com/adam/)" class="btn" id="wa"><i class="fa-brands fa-whatsapp"></i> <br> Klaim Promo Sekarang</a>`);
+    let button = document.write(`<a href="https://api.whatsapp.com/send?phone=6281246776643&text=Halo%20Kak%20Adam%2C%20saya%20dapat%20iklan%20dari%20website%2C%20mau%20tanya%20harga%20terbaru%20Yamaha%20Gear%20(https://gear125.yamahabismagroup.com/adam/)" class="btn" id="wa"><i class="fa-brands fa-whatsapp"></i> Klaim Promo Sekarang</a>`);
     document.getElementById("wa").appendChild(button);
 
 } else {
